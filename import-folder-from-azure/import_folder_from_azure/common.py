6c9a5b0fd33a5a1dbf0617a490cc227f419b685a
Heyi Tang <heta@microsoft.com>
2019-08-21 17:05:43 +0800

    Add logger for logging

--- import-folder-from-azure/import_folder_from_azure/common.py
+++ import-folder-from-azure/import_folder_from_azure/common.py
@@ -1,7 +1,14 @@
 import os
+import logging
 
 from ruamel import yaml
 from azure.storage.blob import BlockBlobService
+
+logger = logging.getLogger('Downloader')
+logger.setLevel(logging.DEBUG)
+hdl = logging.StreamHandler()
+hdl.setFormatter(logging.Formatter('%(asctime)s %(name)-10s %(levelname)-10s %(message)s'))
+logger.addHandler(hdl)
 
 
 class BlobDownloader:
@@ -10,6 +17,7 @@ class BlobDownloader:
 
     def __init__(self, account_name, account_key):
         self.blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
+        self.logger = logger
 
     def download_files_from_blob(self, container, blob_paths, prefix_len, output_folder):
         files = []
@@ -19,17 +27,17 @@ class BlobDownloader:
             folder = os.path.dirname(os.path.join(output_folder, file_name))
             os.makedirs(folder, exist_ok=True)
 
-            print(f"Start downloading file: {file_name}")
+            self.logger.info(f"Start downloading file: {file_name}")
             self.blob_service.get_blob_to_path(
                 container_name=container,
                 blob_name=blob_path,
                 file_path=os.path.join(output_folder, file_name),
             )
-            print(f"End downloading file: {file_name}")
+            self.logger.info(f"End downloading file: {file_name}")
             files.append(file_name)
         if len(files) == 0:
             raise Exception("File not found in path")
-        print(f"{len(files)} files downloaded")
+        self.logger.info(f"{len(files)} files downloaded")
 
     def import_folder(self, blob_path, output_folder, folder_type='GenericFolder'):
         parts = blob_path.strip(self._URL_DELIMITER).split(self._URL_DELIMITER)
@@ -37,7 +45,7 @@ class BlobDownloader:
         blob_path = self._URL_DELIMITER.join(parts[1:]) + self._URL_DELIMITER
         if blob_path == '/':
             blob_path = ''
-        print(f"Blob path parsed: container={container}, path={blob_path}")
+        self.logger.info(f"Blob path parsed: container={container}, path={blob_path}")
         blob_paths = self.blob_service.list_blob_names(container, prefix=blob_path)
         self.download_files_from_blob(container, blob_paths, len(blob_path), output_folder)
         create_if_not_exist = folder_type != 'GenericFolder'
@@ -65,4 +73,4 @@ class BlobDownloader:
         data = {'type': folder_type}
         with open(meta_file_path, 'w') as fout:
             yaml.round_trip_dump(data, fout)
-        print(f"Meta file created: {meta_file_path}")
+        logger.info(f"Meta file created: {meta_file_path}")
