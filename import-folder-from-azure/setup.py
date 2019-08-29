55e0378c2e04beb2c2e38565700061893ffa428b
Heyi Tang <heta@microsoft.com>
2019-08-29 10:35:20 +0800

    Update setup.py

--- import-folder-from-azure/setup.py
+++ import-folder-from-azure/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="azureml-custom-module-import-folder-from-azure",
-    version="0.0.2",
+    version="0.0.3",
     description="Custom modules for importing folder from azure.",
     packages=["import_folder_from_azure"],
     author="Heyi Tang",
